eaa227d42f30d308b7edb97b8d7055107ea37522
Arne Huang <arne@elements.io>
2024-03-31 17:33:59 -0700

    better model validations and errors

--- team_members/views.py
+++ team_members/views.py
@@ -1,33 +1,48 @@
-from rest_framework import generics
-from rest_framework.generics import CreateAPIView, DestroyAPIView, RetrieveAPIView, UpdateAPIView
+from rest_framework import status
+from rest_framework.exceptions import ValidationError
+from rest_framework.generics import CreateAPIView, DestroyAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 from .models import TeamMember
 from .serializers import TeamMemberSerializer
 
 
-class TeamMemberListView(generics.ListAPIView):
+class ValidationErrorMixin:
+    def handle_validation_error(self, exc):
+        return Response({'error': exc.detail}, status=status.HTTP_400_BAD_REQUEST)
+
+    def handle_exception(self, exc):
+        if isinstance(exc, ValidationError):
+            return self.handle_validation_error(exc)
+        return super().handle_exception(exc)
+
+
+class TeamMemberListView(ValidationErrorMixin, ListAPIView):
     queryset = TeamMember.objects.all()
     serializer_class = TeamMemberSerializer
+    pagination_class = PageNumberPagination
+    page_size = 1
 
 
-class TeamMemberDetailView(RetrieveAPIView):
+class TeamMemberDetailView(ValidationErrorMixin, RetrieveAPIView):
     queryset = TeamMember.objects.all()
     serializer_class = TeamMemberSerializer
     lookup_field = 'pk'
 
 
-class TeamMemberCreateView(CreateAPIView):
+class TeamMemberCreateView(ValidationErrorMixin, CreateAPIView):
     queryset = TeamMember.objects.all()
     serializer_class = TeamMemberSerializer
 
 
-class TeamMemberUpdateView(UpdateAPIView):
+class TeamMemberUpdateView(ValidationErrorMixin, UpdateAPIView):
     queryset = TeamMember.objects.all()
     serializer_class = TeamMemberSerializer
     lookup_field = 'pk'
 
 
-class TeamMemberDeleteView(DestroyAPIView):
+class TeamMemberDeleteView(ValidationErrorMixin, DestroyAPIView):
     queryset = TeamMember.objects.all()
     serializer_class = TeamMemberSerializer
     lookup_field = 'pk'
